2b2368865743e4b9475c72be3bff13eb18e37e47
Leonid <leonid_tsyba@mail.ru>
2018-10-01 21:56:05 +0300

    add

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonid/Projects/ROS_prj_norm/src
+# Build directory: /home/leonid/Projects/ROS_prj_norm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(talker)
---
+++ build/talker/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leonid/Projects/ROS_prj_norm/src/talker
+# Build directory: /home/leonid/Projects/ROS_prj_norm/build/talker
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
